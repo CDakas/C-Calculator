293aa72b072658362db41237b3e2d3086d7663e8
CDakas <thegoldeneyed@hotmail.com>
2025-07-06 15:21:42 +0200

    Add files via upload

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -1,63 +1,51 @@
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
 int main()
 {
-
-    int number1, number2, result;
+    int number1, number2;
     char op;
 
+    cout << "Welcome to the simple Calculator, enter two numbers right now!\n";
 
-    std::cout << "Welcome to the simple Calculator, enter two numbers right now!\n";
-    std::cout << "Number 1: \n";
-    std::cin >> number1;
-    std::cout << "Number 2: \n";
-    std::cin >> number2;
-    std::cout << "Now enter one of the following: +, -, *, /!\n";
-    std::cin >> op;
+    cout << "Number 1: \n";
+    while (!(cin >> number1)) {
+        cout << "Invalid input. Please enter an integer for Number 1: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
 
+    cout << "Number 2: \n";
+    while (!(cin >> number2)) {
+        cout << "Invalid input. Please enter an integer for Number 2: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    cout << "Now enter one of the following: +, -, *, /!\n";
+    cin >> op;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clear input buffer
 
     if (op == '+') {
-
-        result = number1 + number2;
-        std::cout << "The Result is: \n" << result;
-
+        cout << "The Result is: \n" << (number1 + number2) << endl;
     }
     else if (op == '-') {
-
-        result = number1 - number2;
-        std::cout << "The Result is: \n" << result;
-
+        cout << "The Result is: \n" << (number1 - number2) << endl;
     }
     else if (op == '*') {
-
-        result = number1 * number2;
-        std::cout << "The Result is: \n" << result;
-
+        cout << "The Result is: \n" << (number1 * number2) << endl;
     }
     else if (op == '/') {
-
-        result = number1 / number2;
-        std::cout << "The Result is: \n" << result;
-
+        if (number2 == 0) {
+            cout << "Error: Division by zero is not allowed.\n";
+        } else {
+            cout << "The Result is: \n" << (static_cast<double>(number1) / number2) << endl;
+        }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    else {
+        cout << "Error: Invalid operator entered.\n";
+    }
 }
 
